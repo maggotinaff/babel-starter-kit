0b2920185df9c482979f7d5aaf745873690ba049
Andrey Skumin <maggot@MacBook-Pro-Andrey.local>
2016-11-11 00:53:32 +0200

    task2c

--- src/index.js
+++ src/index.js
@@ -9,6 +9,28 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/task2c', (req, res) => {
+  const userError = 'Invalid username';
+  const removeText = (str, substr, index) => {
+    let arr = str.split(substr);
+    if (arr.length > 1) {
+      return arr[index];
+    }
+    return str;
+  }
+
+  const getUsername = (username = userError) => {
+    username = removeText(username, '.com/', 1);
+    username = removeText(username, '.xn--p1ai/', 1);
+    username = removeText(username, '?', 0);
+    username = removeText(username, '@', 1);
+    username = removeText(username, '/', 0);
+    return username === userError ? userError : `@${username}`;
+  }
+
+  res.send(getUsername(req.query.username));
+});
+
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
